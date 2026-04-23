29ce3611a6de8629ec11d782c374ce4e5061b9e0
OanaM <opopitiu@opentext.com>
2026-04-23 11:18:12 +0300

    update script

--- scripts/TruClient2_1/Action.c
+++ scripts/TruClient2_1/Action.c
@@ -4,11 +4,11 @@
 
 Action()
 {
-	lr_start_transaction("Transaction 1");
+	lr_start_transaction("Transaction 2");
 	truclient_step("1", "Navigate to http://opentext.com/", "snapshot=Action_1.inf");
 	truclient_step("2", "Click on Accept All Button", "snapshot=Action_2.inf");
 	truclient_step("4", "Click on Products Focusable", "snapshot=Action_4.inf");
 	truclient_step("6", "Click on Application Modernization Link", "snapshot=Action_6.inf");
-	lr_end_transaction("Transaction 1",0);
+	lr_end_transaction("Transaction 2",0);
 	return 0;
 }
